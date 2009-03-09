a63678d570f0b7688612580ede4f201ca6fde4be
trueblue28 <trueblue28@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-03-09 02:19:10 +0000

    adding build structure around dropkick

---
+++ open.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+
+set SOLUTION=dropkick.sln
+start /b "%PROGRAMFILES%\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" %SOLUTION% /Edit
---
+++ test.bat
@@ -0,0 +1,3 @@
+.\lib\tools\Nant\nant.exe /f:.\build\_compile.build
+.\lib\tools\Nant\nant.exe /f:.\build\_test.build %1
+.\lib\tools\Nant\nant.exe /f:.\build\_test.build open_results
