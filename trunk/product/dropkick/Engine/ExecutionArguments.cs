598e689df872289a9fee589415b80827ca73969c
dru.sellers <dru.sellers@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-06-16 04:36:48 +0000

    the abuse of the curry concept to have fun while parsing command line arguments

--- trunk/product/dropkick/Engine/ExecutionArguments.cs
+++ trunk/product/dropkick/Engine/ExecutionArguments.cs
@@ -1,9 +1,16 @@
 namespace dropkick.Engine
 {
+    using System;
+    using System.Text.RegularExpressions;
     using Dsl;
+    using Visitors.Trace;
 
     public class ExecutionArguments
     {
+        public ExecutionArguments()
+        {
+            Inspector = new TraceVisitor();
+        }
         public string Environment { get; set; }
         public string Part { get; set; }
         public string DeploymentAssembly { get; set; }
