94f56d65ebdd41b1da06d53cb351522f30defd5e
dru.sellers <dru.sellers@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-06-16 04:50:53 +0000

    some more tweaks

--- trunk/product/dropkick.console/Program.cs
+++ trunk/product/dropkick.console/Program.cs
@@ -6,17 +6,18 @@
     {
         static void Main(string[] args)
         {
-            //get environment --- /e:local|dev|test|qa|prod|dr
-            //get part/partCfg -- /p:web|app|service
-            //get 'Deployment' class from assembly
-            //                 -- /d:FHLBank.Flames.Deployment.FlamesDeployment (or there could just be one?)
+            // get environment -- /e:local|dev|test|qa|prod|dr
+            // get part        -- /p:web|app|service
+            // get 'Deployment' class from assembly
+            // assumes only one deployment class
+            //                -- /d:FHLBank.Flames.Deployment
             
             // -v to verify
             // -x to execute
             // default is trace
             var a = ArgumentParsing.Parse(args);
 
-            new Runner(new AssumesOnlyOneDeploymentFinder(a.DeploymentAssembly)).Deploy(a);
+            new Runner(a.DeploymentFinder).Deploy(a);
         }
     }
 }
