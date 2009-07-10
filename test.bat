38cdd5cffa32f1e3df321a63c8d4edfe0ca136fc
ferventcoder <ferventcoder@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-07-10 05:57:13 +0000

    upgrading UppercuT to the most current version.

--- test.bat
+++ test.bat
@@ -13,42 +13,25 @@ if '%1' == '/help' goto usage
 SET DIR=%~d0%~p0%
 
 SET build.config.settings="%DIR%Settings\UppercuT.config"
-"%DIR%lib\tools\Nant\nant.exe" %1 /f:.\BuildScripts\_compile.build -D:build.config.settings=%build.config.settings%
+"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\_compile.build -D:build.config.settings=%build.config.settings%
+"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\analyzers\_test.build %1 -D:build.config.settings=%build.config.settings%
+"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\analyzers\_test.build open_results -D:build.config.settings=%build.config.settings%
 
-:: DEPRECATED
-:: call "%DIR%Settings\build.settings.bat"
-
-::"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\_compile.build -D:project.name=%PROJECT_NAME% -D:microsoft.framework=%MICROSOFT_FRAMEWORK%
-
-:: Use this option if you explicitly want to set the items in _compile.build
-::"%DIR%libs\tools\Nant\nant.exe" /f:.\BuildScripts\_compile.build
-
-if '%TESTING_FRAMEWORK%' == '"mbunit"' goto mbunit
-if '%TESTING_FRAMEWORK%' == '"nunit"' goto nunit
-
-:mbunit
-"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\Analyzers\_MbUnit.build %1 -D:build.config.settings=%build.config.settings%
-"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\Analyzers\_MbUnit.build open_results -D:build.config.settings=%build.config.settings%
 goto bdddoc
 
 :bdddoc
 
 SET APP_BDDDOC="..\lib\testing\bdddoc\bdddoc.console.exe"
 
-%DIR%lib\tools\Nant\nant.exe /f:.\BuildScripts.Custom\_bdddoc.build -D:app.bdddoc=%APP_BDDDOC% -D:build.config.settings=%build.config.settings%
-%DIR%lib\tools\Nant\nant.exe /f:.\BuildScripts.Custom\_bdddoc.build open_results -D:build.config.settings=%build.config.settings%
+"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts.Custom\_bdddoc.build -D:app.bdddoc=%APP_BDDDOC% -D:build.config.settings=%build.config.settings%
+"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts.Custom\_bdddoc.build open_results -D:build.config.settings=%build.config.settings%
 
-goto finish
-
-:nunit
-"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\Analyzers\_nunit.build %1 -D:build.config.settings=%build.config.settings%
-"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\Analyzers\_nunit.build open_results -D:build.config.settings=%build.config.settings%
 goto finish
 
 :usage
 echo.
 echo Usage: test.bat
-echo Usage: test.bat run_all_tests to run all tests
+echo Usage: test.bat all - to run all tests
 echo.
 goto finish
 
--- zip.bat
+++ zip.bat
@@ -9,9 +9,3 @@ call "%DIR%build.bat"
 
 SET build.config.settings="%DIR%Settings\UppercuT.config"
 "%DIR%lib\tools\Nant\nant.exe" %1 /f:.\BuildScripts\_zip.build -D:build.config.settings=%build.config.settings%
-
-:: DEPRECATED
-::"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\_zip.build -D:project.name=%PROJECT_NAME%
-:: Use this option if you explicitly want to set the items in _zip.build
-
-::%DIR%lib\tools\Nant\nant.exe /f:.\BuildScripts\_update_assemblies.build
