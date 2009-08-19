f12e5c1faab9537074e4ee6a3da2215c488c61e2
dru.sellers <dru.sellers@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-08-19 01:33:44 +0000

    improving testing

--- trunk/product/dropkick/Configuration/Dsl/Dsn/DsnDriver.cs
+++ trunk/product/dropkick/Configuration/Dsl/Dsn/DsnDriver.cs
@@ -2,7 +2,16 @@ namespace dropkick.Dsl.Dsn
 {
     public class DsnDriver
     {
-        private string value;
-        
+        public static DsnDriver Access()
+        {
+            return new DsnDriver(){Value = "Microsoft Access Driver (*.MDB)\0"};
+        }
+
+        public static DsnDriver Sql()
+        {
+            return new DsnDriver(){Value="SQL Server"};
+        }
+
+        public string Value { get; private set; }
     }
 }
