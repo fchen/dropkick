9d3f133129e8355bf1ab6d96c00af61fc274ddbe
dru.sellers <dru.sellers@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-09-23 12:43:26 +0000

    fussing with naming still

--- trunk/product/dropkick.tests/Scenarios/CommandLineScenario.cs
+++ trunk/product/dropkick.tests/Scenarios/CommandLineScenario.cs
@@ -9,20 +9,12 @@ namespace dropkick.tests.Scenarios
     public class CommandLineScenario :
         Scenario
     {
-        ExecutionArguments _verifyArguments;
         DropkickDeploymentInspector _interpreter;
 
         [Test]
         public void Verify()
         {
             _interpreter = new DropkickDeploymentInspector();
-            _verifyArguments = new ExecutionArguments()
-            {
-                Deployment = GetType().Assembly.FullName,
-                Environment = "TEST",
-                Command = DropkickCommands.Verify,
-                Part = "WEB"
-            };
 
             var dep = new GenericDeploymentFinder<CommandTestDeploy>().Find("");
             dep.Inspect(_interpreter);
