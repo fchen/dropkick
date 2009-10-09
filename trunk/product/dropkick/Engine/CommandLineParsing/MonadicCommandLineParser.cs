dd17753d7295f8e26a3ec6e3d292b27232bca388
dru.sellers <dru.sellers@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-10-09 00:37:56 +0000

    working on some commandline love

--- trunk/product/dropkick/Engine/CommandLineParsing/MonadicCommandLineParser.cs
+++ trunk/product/dropkick/Engine/CommandLineParsing/MonadicCommandLineParser.cs
@@ -1,18 +1,55 @@
 namespace dropkick.Engine.CommandLineParsing
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Magnum.CommandLineParser;
 
     public static class MonadicCommandLineParser
     {
         public static DeploymentArguments Parse(string commandline)
         {
+            var result = new DeploymentArguments();
+
+            Set(result, P(commandline));
+
             return new DeploymentArguments();
+        }
+
+        static void Set(DeploymentArguments arguments, IEnumerable<ICommandLineElement> commandLineElements)
+        {
+            var command = commandLineElements.Where(x => x is IArgumentElement)
+                .Select(x => (IArgumentElement) x)
+                .DefaultIfEmpty(new ArgumentElement("trace"))
+                .SingleOrDefault();
+
+            arguments.Command = command.Id.ToEnum<DropkickCommands>();
+
+            
+            var deployment = commandLineElements.Where(x => x is IDefinitionElement)
+                .Select(x => x as IDefinitionElement)
+                .Where(x => x.Key == "d")
+                .DefaultIfEmpty(new DefinitionElement("d","value"))
+                .SingleOrDefault();
+
+            arguments.Deployment = deployment.Value;
+
+
+            var part = commandLineElements.Where(x => x is IDefinitionElement)
+                .Select(x => x as IDefinitionElement)
+                .Where(x => x.Key == "p")
+                .DefaultIfEmpty(new DefinitionElement("p", "ALL"))
+                .SingleOrDefault();
+
+            arguments.Part = part.Value;
+
+
         }
 
         static IEnumerable<ICommandLineElement> P(string commandLine)
         {
             var parser = new Magnum.CommandLineParser.MonadicCommandLineParser();
+
             return parser.Parse(commandLine);
         }
     }
--- trunk/product/dropkick/Extensions.cs
+++ trunk/product/dropkick/Extensions.cs
@@ -1,10 +1,16 @@
 namespace dropkick
 {
+    using System;
+
     public static class Extensions
     {
         public static string FormatWith(this string input, params object[] args)
         {
             return string.Format(input, args);
+        }
+        public static T ToEnum<T>(this string input)
+        {
+            return (T) Enum.Parse(typeof(T), input, true);
         }
     }
 }
