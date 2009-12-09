38155edc7c9910dbb34bf4c318543361dae7f7a2
dru.sellers <dru.sellers@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2009-12-09 13:12:19 +0000

    odd changes - vs is silly

--- product/dropkick.tests/Configuration/Dsl/Verification_Demos.cs
+++ product/dropkick.tests/Configuration/Dsl/Verification_Demos.cs
@@ -1,6 +1,18 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace dropkick.tests.Configuration.Dsl
 {
-    using Engine;
+    using dropkick.Engine;
     using NUnit.Framework;
     using TestObjects;
 
@@ -8,12 +20,12 @@ namespace dropkick.tests.Configuration.Dsl
     [Category("Integration")]
     public class Verification_Demos
     {
-        DeploymentArguments _verifyArguments;
+        #region Setup/Teardown
 
         [SetUp]
         public void Setup()
         {
-            _verifyArguments = new DeploymentArguments()
+            _verifyArguments = new DeploymentArguments
                                {
                                    Deployment = GetType().Assembly.FullName,
                                    Environment = "TEST",
@@ -28,10 +40,23 @@ namespace dropkick.tests.Configuration.Dsl
         {
         }
 
+        #endregion
+
+        DeploymentArguments _verifyArguments;
+
         [Test]
-        public void Verify_MsSql_Test()
+        public void Verify_Command()
         {
-            var dep = new MsSqlTestDeploy();
+            Setup();
+            var dep = new CommandTestDeploy();
+            DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void Verify_Iis()
+        {
+            var dep = new IisTestDeploy();
             DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
         }
 
@@ -43,27 +68,10 @@ namespace dropkick.tests.Configuration.Dsl
         }
 
         [Test]
-        public void Verify_Command()
+        public void Verify_MsSql_Test()
         {
-            Setup();
-            var dep = new CommandTestDeploy();
+            var dep = new MsSqlTestDeploy();
             DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
-        }
-
-        [Test]
-        public void Verify_WinService()
-        {
-            var dep = new WinServiceTestDeploy();
-            DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
-        }
-
-        [Test]
-        [Category("Integration")]
-        public void Verify_Iis()
-        {
-            var dep = new IisTestDeploy();
-            DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
-            
         }
 
 
@@ -72,7 +80,13 @@ namespace dropkick.tests.Configuration.Dsl
         {
             var dep = new TestDeployment();
             DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
-            
+        }
+
+        [Test]
+        public void Verify_WinService()
+        {
+            var dep = new WinServiceTestDeploy();
+            DeploymentPlanDispatcher.KickItOutThereAlready(dep, _verifyArguments);
         }
     }
 }
--- product/dropkick.tests/Configuration/Dsl/WithTwoPartDeployContext.cs
+++ product/dropkick.tests/Configuration/Dsl/WithTwoPartDeployContext.cs
@@ -1,7 +1,19 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace dropkick.tests.Configuration.Dsl
 {
     using dropkick.Configuration.Dsl;
-    using Engine;
+    using dropkick.Engine;
     using NUnit.Framework;
     using TestObjects;
 
@@ -18,9 +30,9 @@ namespace dropkick.tests.Configuration.Dsl
             Deployment = new TwoPartDeploy();
             Inspector = new DropkickDeploymentInspector();
             Map = new RoleToServerMap();
-            Map.AddMap("Web","SrvTopeka09");
-            Map.AddMap("Web","SrvTopeka19");
-            Map.AddMap("Db","SrvTopeka02");
+            Map.AddMap("Web", "SrvTopeka09");
+            Map.AddMap("Web", "SrvTopeka19");
+            Map.AddMap("Db", "SrvTopeka02");
 
             BecauseOf();
         }
