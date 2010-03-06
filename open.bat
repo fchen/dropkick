8e274a8de7bc8f9f04553260e38b38cb0b3b4ac8
ferventcoder <ferventcoder@7beece84-0b2a-11de-b17c-0f1ef23b492c>
2010-03-06 04:01:37 +0000

    updating build and restructuring the project a little

--- open.bat
+++ open.bat
@@ -4,7 +4,7 @@
 ::No edits to this file are required - http://uppercut.pbwiki.com
 
 SET DIR=%~d0%~p0%
-SET NANT="%DIR%lib\tools\Nant\nant.exe"
+SET NANT="%DIR%lib\Nant\nant.exe"
 SET build.config.settings="%DIR%settings\UppercuT.config"
 
 %NANT% %1 /f:.\build\open.build -D:build.config.settings=%build.config.settings%
--- zip.bat
+++ zip.bat
@@ -4,12 +4,11 @@
 ::No edits to this file are required - http://uppercut.pbwiki.com
 
 SET DIR=%~d0%~p0%
-
+::This is gone once teamcity can call multiple files
 call "%DIR%build.bat"
-
 if %ERRORLEVEL% NEQ 0 goto errors
 
-SET NANT="%DIR%lib\tools\Nant\nant.exe"
+SET NANT="%DIR%lib\Nant\nant.exe"
 SET build.config.settings="%DIR%settings\UppercuT.config"
 
 %NANT% %1 /f:.\build\zip.build -D:build.config.settings=%build.config.settings%
